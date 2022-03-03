bce71251c6821d8eae3d33a99e1d299917313f3f
Tanishq Singh <>
2022-03-03 21:33:08 +0530

    next.config.js file updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
 
-const nextConfig = {
-  reactStrictMode: true,
+const pwaConfig = {
   pwa: {
     dest: 'public',
     sw: 'service-worker.js'
   }
 }
 
-module.exports = withPWA(nextConfig);
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+if (process.env.DEV == 'true')
+  module.exports = nextConfig;
+
+else
+  module.exports = withPWA({
+    ...nextConfig,
+    ...pwaConfig
+  });
